13c561bb3dc528851144fe61a5d1a5cac468f9f1
athebolt <themanwiththeplan01@icloud.com>
2023-10-02 15:08:55 -0400

    Initiated Project
    Project Created

---
+++ DatabaseExample/app/src/main/java/com/example/databaseexample/User.java
@@ -0,0 +1,50 @@
+package com.example.databaseexample;
+
+public class User
+{
+    private String fname;
+    private String lname;
+    private String uname;
+
+    public User()
+    {
+
+    }
+
+    public User(String f, String l, String u)
+    {
+        fname = f;
+        lname = l;
+        uname = u;
+    }
+
+    public String getFname()
+    {
+        return fname;
+    }
+
+    public void setFname(String f)
+    {
+        fname = f;
+    }
+
+    public String getLname()
+    {
+        return lname;
+    }
+
+    public void setLname(String l)
+    {
+        lname = l;
+    }
+
+    public String getUname()
+    {
+        return uname;
+    }
+
+    public void setUname(String u)
+    {
+        uname = u;
+    }
+}
