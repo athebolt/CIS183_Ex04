07de1c3ddf8c8c17e518973e24780ee668994589
athebolt <themanwiththeplan01@icloud.com>
2023-10-09 14:52:23 -0400

    Added Delete user and add user
    Added
    -Delete user
    -Create user
    -Update user

--- DatabaseExample/app/src/main/java/com/example/databaseexample/MainActivity.java
+++ DatabaseExample/app/src/main/java/com/example/databaseexample/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity
     ListView lv_j_users;
     ArrayList<User> userList;
     DatabaseHelper dbHelper;
+    ArrayList<String> usernames;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -45,9 +49,22 @@ public class MainActivity extends AppCompatActivity
         //we should see 4 when we run this
         Log.d("Number of records: ", dbHelper.numberOfRowsInTable() + "");
         userList = dbHelper.getAllRows();
-        displayUsers();
+
+        //for testing only
+        //displayUsers();
+
+        //get all of the usernames from our table
+        usernames = dbHelper.getAllUsernames();
+
+        //Remember that this is a simple menu, mean you can only display one string per cell
+        //In order to display multiple items in a cell, a custom adapter and cell need to be created
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, usernames);
+
+        //tell the listview to use the adapter
+        lv_j_users.setAdapter(adapter);
 
         addNewUserButtonEvent();
+        deleteUserEvent();
     }
 
     public void addNewUserButtonEvent()
@@ -57,17 +74,33 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-                //Log.d("Button pressed", "Add New User");
+                Log.d("Button pressed", "Add New User");
 
                 String u = et_j_uname.getText().toString();
                 String f = et_j_fname.getText().toString();
                 String l = et_j_lname.getText().toString();
 
+                //creating a new user with information
                 User user = new User(u, f, l);
 
+                //adding user to the database and the arraylist
                 addNewUser(user);
 
-                displayUsers();
+                //add the username to the username arraylist
+                usernames.add(u);
+
+                //this line is easily forgotten
+                //this line is needed so the listview will reflect the new user based off the new
+                //username added to the usernames arraylist.
+                adapter.notifyDataSetChanged();
+
+                //this is for testing purposes
+                //displayUsers();
+
+                //clear textboxes
+                et_j_fname.setText("");
+                et_j_lname.setText("");
+                et_j_uname.setText("");
 
                 //add to database
                 //save to an array list
@@ -78,7 +111,11 @@ public class MainActivity extends AppCompatActivity
 
     public void addNewUser(User u)
     {
+        //add user to array list
         userList.add(u);
+
+        //add user to database
+        dbHelper.addNewUser(u);
     }
 
     public void displayUsers()
@@ -87,5 +124,27 @@ public class MainActivity extends AppCompatActivity
         {
             Log.d("User: ", userList.get(i).getFname());
         }
+    }
+
+    public void deleteUserEvent()
+    {
+        lv_j_users.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+            {
+                //call the delete function in our dbHelper and pass it username
+                dbHelper.deleteUser(usernames.get(i));
+
+                //remove the user from our userlist
+                userList.remove(i);
+
+                //remove user from our usernames
+                usernames.remove(i);
+
+                //update the listview to see the changes
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
